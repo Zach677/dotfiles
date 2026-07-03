d2840d97b37e026c5d7d69f46509ae0ee0cdc2a7
Zach <2801818616z@gmail.com>
2026-07-03 20:18:19 +0800

    Update Bitwarden SSH agent socket path

--- tag-base/config/zsh/00-env.zsh
+++ tag-base/config/zsh/00-env.zsh
@@ -16,7 +16,7 @@ if command -v tty >/dev/null 2>&1 && [[ -t 0 ]]; then
     export GPG_TTY="$(tty)"
 fi
 
-bitwarden_ssh_sock="$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
+bitwarden_ssh_sock="$HOME/.bitwarden-ssh-agent.sock"
 if [[ -S "$bitwarden_ssh_sock" ]]; then
     export SSH_AUTH_SOCK="$bitwarden_ssh_sock"
     command -v launchctl >/dev/null 2>&1 && launchctl setenv SSH_AUTH_SOCK "$SSH_AUTH_SOCK" >/dev/null 2>&1
